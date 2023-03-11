3cdeb4d7b4d716dda1f4d5a8fdd74d012de2273c
Rahul Sharan <123796550+rahuls512@users.noreply.github.com>
2023-03-11 16:20:07 +0530

    Add files via upload

---
+++ Project_Greeting user.py
@@ -0,0 +1,7 @@
+#project that uses Python's print function, strings, input function, and variables:
+#Project: Greeting User
+
+print("Welcome to Here..")
+user_name = input("What's your name?\n ")
+greeting = "Hello!!" 
+print("Hi " + user_name + " " + greeting)

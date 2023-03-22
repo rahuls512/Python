bdf494252c01edb2639cf53ac00d838e54e5e7ad
Rahul Sharan <123796550+rahuls512@users.noreply.github.com>
2023-03-22 10:11:43 +0530

    Delete input() function.py

--- input() function.py
+++
@@ -1,9 +0,0 @@
-#Input Function:
-#The input function is used to accept user input in Python. 
-#It displays a message to the user and waits for them to enter a value
-
-print( len( input("What is your name? ") ) )
-
-name = input("What is your name? ")
-length = (name)
-print(length)

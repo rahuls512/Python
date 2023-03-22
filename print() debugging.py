118abc6c1476acd75eedb5142b9f6b144848fc74
Rahul Sharan <123796550+rahuls512@users.noreply.github.com>
2023-03-22 10:11:58 +0530

    Delete print() debugging.py

--- print() debugging.py
+++
@@ -1,9 +0,0 @@
-#After you have written your code, you should run your program and it should print the following:
-
-#Day 1 - Python Print Function
-#The function is declared like this:
-#print('what to print')
-
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
